21d625c1d06b12eed922d4945b71009b0fd94e0d
hsamani <123456@aA>
2022-09-03 15:26:56 +0430

    price added

--- src/composable/basket.js
+++ src/composable/basket.js
@@ -1,34 +1,62 @@
-import { computed } from "vue";
 import { useProductsStore } from "@/store/products";
 
 export default async function (editor) {
-  const store = useProductsStore();
   const domComponent = editor.DomComponents;
 
-  const isAvailable =
-    store.product.order_available && store.product.order_no_check > 0
-      ? "موجود"
-      : "ناموجود";
+  const store = useProductsStore();
 
-  const basektComponent = `
-  <div>
-    ${isAvailable}
-    <hr />
+  const basketComponent = (v) => {
+    const { price } = v;
+    return `
+    <div class="wrapper">
+      <hr />
         <div>
             <span>قیمت کالا:</span>
-            <div>65880000</div>
+            <div class="price">تومان ${price.toLocaleString("fa-IR")}</div>
+        </div>
+        <div>
+          <button class="add_to_basket">افزودن به سبد خرید</button>
         </div>
     </div>`;
+  };
 
   domComponent.addType("basket", {
     model: {
       defaults: {
-        components: basektComponent,
+        styles: `.wrapper {
+          width: 278px;
+          text-align: right;
+          background-color: #e2e2e2;
+          padding: 12px 14px 20px 14.5px;
+          border: solid 1px #e2e2e2;
+          border-radius: 8px;
+        }
+        
+        .price {
+          text-align:left;
+          font-size: 18px;
+          font-weight: bold;
+          font-stretch: normal;
+          font-style: normal;
+          line-height: 1.67;
+          letter-spacing: normal;
+          text-align: left;
+          color: #fc406c;
+        }     
+        
+        .add_to_basket {
+          width:100%;
+          background-color: #fc406c;
+          color: #ffffff;
+          border: none;
+        }
+        `,
       },
+
+      // not on client side, only dashboard
       async init() {
-        await store.getProductWithId();
-        const res = computed(() => store.product);
-        res.value.forEach(({ body }) => this.append(body));
+        const res = await store.getProductWithId();
+        this.components(basketComponent(res));
       },
     },
   });
