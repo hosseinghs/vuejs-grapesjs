f494c90e1f09a039034642696a0e666f1f078ebc
hsamani <123456@aA>
2022-09-04 12:46:54 +0430

    unneeded code deleted

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,26 +5,8 @@ export default {
 </script>
 
 <script setup>
-// import { computed } from "vue";
-// import Text from "@/components/Text.vue";
-import { useProductsStore } from "@/store/products";
-
-const store = useProductsStore();
-
-function getData() {
-  store.getProducts();
-}
 </script>
 
 <template>
-  <section>
-    <!-- <Text
-      v-for="{ id, postId, body } in comments"
-      :key="id"
-      :id="id"
-      :post-id="postId"
-      :body="body"
-    /> -->
-    <button @click="getData">add data</button>
-  </section>
+  <section></section>
 </template>
